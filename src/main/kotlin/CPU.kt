72b67c85626ab8ca2d327c5ed338b15cd21bb42c
Thomas Prebeck <thomas.prebeck@oth-regensburg.de>
2017-12-04 15:39:14 +0100

    opcodes...

--- src/main/kotlin/CPU.kt
+++ src/main/kotlin/CPU.kt
@@ -12,6 +12,9 @@ class CPU() {
   private var SP: Int = 0
   private var PC: Int = 0
   private val stack: HashMap<Int, Int> = HashMap()
+  private val running = true
+  private val interrupt = true
+  private val prefix = false
 
   private val ram = RAM()
 
@@ -244,7 +247,13 @@ class CPU() {
     val a = A.toString(2).padStart(8, '0')
     val c = getCarry()
     setCarry(a.first())
-    return (a.substring(1) + c).toShort(2)
+    val ret = (a.substring(1) + c).toShort(2)
+    if (prefix) {
+      if (ret.toInt() == 0) {
+        setZero('1')
+      }
+    }
+    return ret
   }
   private fun rlc(A: Short): Short {
     setZero('0')
@@ -252,7 +261,13 @@ class CPU() {
     setSubstract('0')
     val a = A.toString(2).padStart(8, '0')
     setCarry(a.first())
-    return (a.substring(1) + a.first()).toShort(2)
+    val ret = (a.substring(1) + a.first()).toShort(2)
+    if (prefix) {
+      if (ret.toInt() == 0) {
+        setZero('1')
+      }
+    }
+    return ret
   }
 
   private fun rr(A: Short): Short {
@@ -262,7 +277,13 @@ class CPU() {
     val a = A.toString(2).padStart(8, '0')
     val c = getCarry()
     setCarry(a.last())
-    return (c + a.substring(1)).toShort(2)
+    val ret = (c + a.substring(1)).toShort(2)
+    if (prefix) {
+      if (ret.toInt() == 0) {
+        setZero('1')
+      }
+    }
+    return ret
   }
   private fun rrc(A: Short): Short {
     setZero('0')
@@ -270,8 +291,88 @@ class CPU() {
     setSubstract('0')
     val a = A.toString(2).padStart(8, '0')
     setCarry(a.last())
-    return (a.last() + a.substring(0, a.length - 1)).toShort(2)
+    val ret = (a.last() + a.substring(0, a.length - 1)).toShort(2)
+    if (prefix) {
+      if (ret.toInt() == 0) {
+        setZero('1')
+      }
+    }
+    return ret
   }
+
+  private fun sla(A: Short): Short {
+    setZero('0')
+    setHalfCarry('0')
+    setSubstract('0')
+    val a = A.toString(2).padStart(8, '0')
+    setCarry(a.first())
+    val ret = (a.substring(1) + "0").toShort(2)
+    if (ret.toInt() == 0) {
+      setZero('1')
+    }
+    return ret
+  }
+  private fun sra(A: Short): Short {
+    setZero('0')
+    setHalfCarry('0')
+    setSubstract('0')
+    val a = A.toString(2).padStart(8, '0')
+    setCarry(a.last())
+    val ret = (a.first() + a.substring(0, a.length - 1)).toShort(2)
+    if (ret.toInt() == 0) {
+      setZero('1')
+    }
+    return ret
+  }
+  private fun srl(A: Short): Short {
+    setZero('0')
+    setHalfCarry('0')
+    setSubstract('0')
+    val a = A.toString(2).padStart(8, '0')
+    setCarry(a.last())
+    val ret = ('0' + a.substring(0, a.length - 1)).toShort(2)
+    if (ret.toInt() == 0) {
+      setZero('1')
+    }
+    return ret
+  }
+
+  private fun swap(A: Short): Short {
+    setZero('0')
+    setHalfCarry('0')
+    setSubstract('0')
+    setCarry('0')
+    val a = A.toString(2).padStart(8, '0')
+    val ret = (a.substring(4) + a.substring(0, 4)).toShort(2)
+    if (ret.toInt() == 0) {
+      setZero('1')
+    }
+    return ret
+  }
+
+  private fun BIT(A: Short, c: Int): Short {
+    val a = A.toString(2).padStart(8, '0')
+    val ret = a.get(c)
+    return ret
+  }
+  private fun SET(A: Short, c: Int): Short {
+    val a = A.toString(2).padStart(8, '0')
+    val ret = a.substring(0, c) + '1' + a.substring(c)
+    return ret
+  }
+  private fun RES(A: Short, c: Int): Short {
+    val a = A.toString(2).padStart(8, '0')
+    val ret = a.substring(0, c) + '1' + a.substring(c)
+    return ret
+  }
+
+  private var AF: Int
+    get() = joinHighByteLowByte(A, F)
+    set(x: Int) {
+      val (nb, nc) = splitHighByteLowByte(x)
+      A = nb
+      F = nc
+    }
   private var BC: Int
     get() = joinHighByteLowByte(B, C)
     set(x: Int) {
@@ -371,7 +472,7 @@ class CPU() {
         A = (A - 0xFF.toShort()).toShort()
       }
     },
-    0x28 to {
+    0x28 to { // JP Z, a8
       if (getZero() == '1') {
         PC = PC + ram.getByteAt(PC).toByte().toInt()
       }
@@ -601,9 +702,7 @@ class CPU() {
         PC = getNextWord()
       }
     },
-    0xc3 to {
-      PC = getNextWord()
-    },
+    0xc3 to { PC = getNextWord() },
     0xc4 to {
       if (getZero() == '0') {
         stack.put(SP, PC)
@@ -656,8 +755,386 @@ class CPU() {
       stack.put(SP, PC)
       SP = SP - 2
       PC = joinHighByteLowByte(0.toShort(), 0x8.toShort())
+    },
+    0xd0 to {
+      if (getCarry() == '0') {
+        PC = checkNotNull(stack.get(SP))
+        SP = SP + 2
+      }
+    },
+    0xd1 to {
+      DE = checkNotNull(stack.get(SP))
+      stack.remove(SP)
+      SP = SP + 2
+    },
+    0xd2 to {
+      if (getCarry() == '0') {
+        PC = getNextWord()
+      }
+    },
+    0xd4 to {
+      if (getCarry() == '0') {
+        stack.put(SP, PC)
+        SP = SP - 2
+        PC = getNextWord()
+      }
+    },
+    0xd5 to {
+      stack.put(SP, DE)
+      SP = SP - 2
+    },
+    0xd6 to { A = A SUB ram.getByteAt(PC++) },
+    0xd7 to {
+      stack.put(SP, PC)
+      SP = SP - 2
+      PC = joinHighByteLowByte(0.toShort(), 0x10.toShort())
+    },
+    0xd8 to {
+      if (getCarry() == '1') {
+        PC = checkNotNull(stack.get(SP))
+        SP = SP + 2
+      }
+    },
+    0xd9 to {
+      //Enable interrupts
+      PC = checkNotNull(stack.get(SP))
+      SP = SP + 2
+    },
+    0xda to {
+      if (getCarry() == '1') {
+        PC = getNextWord()
+      }
+    },
+    0xdc to {
+      if (getCarry() == '1') {
+        stack.put(SP, PC)
+        SP = SP - 2
+        PC = getNextWord()
+      }
+    },
+    0xde to { A = (A SBC ram.getByteAt(PC++)) },
+    0xdf to {
+      stack.put(SP, PC)
+      SP = SP - 2
+      PC = joinHighByteLowByte(0.toShort(), 0x18.toShort())
+    },
+    0xe0 to { ram.setByteAt(0x0000FF00 + ram.getByteAt(PC++).toInt(), A) },
+    0xe1 to {
+      HL = checkNotNull(stack.get(SP))
+      stack.remove(SP)
+      SP = SP + 2
+    },
+    0xe2 to { ram.setByteAt(0x0000FF00 + C.toInt(), A) },
+    0xe5 to {
+      stack.put(SP, HL)
+      SP = SP - 2
+    },
+    0xe6 to { A = A AND ram.getByteAt(PC++) },
+    0xe7 to {
+      stack.put(SP, PC)
+      SP = SP - 2
+      PC = joinHighByteLowByte(0.toShort(), 0x20.toShort())
+    },
+    0xe8 to {
+      SP = SP + (ram.getByteAt(PC++).toByte()).toInt()
+    },
+    0xe9 to { PC = HL },
+    0xea to { ram.setByteAt(getNextWord(), A) },
+    0xee to { A = (A XOR ram.getByteAt(PC++)) },
+    0xef to {
+      stack.put(SP, PC)
+      SP = SP - 2
+      PC = joinHighByteLowByte(0.toShort(), 0x28.toShort())
+    },
+    0xf0 to { A = ram.getByteAt(0x0000FF00 + ram.getByteAt(PC++).toInt()) },
+    0xf1 to {
+      AF = checkNotNull(stack.get(SP))
+      stack.remove(SP)
+      SP = SP + 2
+    },
+    0xf2 to { A = ram.getByteAt(0x0000FF00 + C.toInt()) },
+    0xf3 to { println("Disable Interrupt") },
+    0xf5 to {
+      stack.put(SP, AF)
+      SP = SP - 2
+    },
+    0xf6 to { A = A OR ram.getByteAt(PC++) },
+    0xf7 to {
+      stack.put(SP, PC)
+      SP = SP - 2
+      PC = joinHighByteLowByte(0.toShort(), 0x30.toShort())
+    },
+    0xf8 to { HL = SP + (ram.getByteAt(PC++).toByte()).toInt() },
+    0xf9 to { SP = HL },
+    0xfa to { A = ram.getByteAt(getNextWord()) },
+    0xfb to { println("Enable interrupts") },
+    0xfe to {
+      A SUB ram.getByteAt(PC++)
+      Unit
+    },
+    0xff to {
+      stack.put(SP, PC)
+      SP = SP - 2
+      PC = joinHighByteLowByte(0.toShort(), 0x38.toShort())
     }
-
+  )
+  private val prefixOpcodes: Map<Int, Function<Unit>> = mapOf(
+    0x00 to { B = rlc(B) },
+    0x01 to { C = rlc(C) },
+    0x02 to { D = rlc(D) },
+    0x03 to { E = rlc(E) },
+    0x04 to { H = rlc(H) },
+    0x05 to { L = rlc(L) },
+    0x06 to { ram.setByteAt(HL, rlc(ram.getByteAt(HL))) },
+    0x07 to { A = rlc(A) },
+    0x08 to { B = rrc(B) },
+    0x09 to { C = rrc(C) },
+    0x0a to { D = rrc(D) },
+    0x0b to { E = rrc(E) },
+    0x0c to { H = rrc(H) },
+    0x0d to { L = rrc(L) },
+    0x0e to { ram.setByteAt(HL, rrc(ram.getByteAt(HL))) },
+    0x0f to { A = rrc(A) },
+    0x10 to { B = rl(B) },
+    0x11 to { C = rl(C) },
+    0x12 to { D = rl(D) },
+    0x13 to { E = rl(E) },
+    0x14 to { H = rl(H) },
+    0x15 to { L = rl(L) },
+    0x16 to { ram.setByteAt(HL, rl(ram.getByteAt(HL))) },
+    0x17 to { A = rl(A) },
+    0x18 to { B = rr(B) },
+    0x19 to { C = rr(C) },
+    0x1a to { D = rr(D) },
+    0x1b to { E = rr(E) },
+    0x1c to { H = rr(H) },
+    0x1d to { L = rr(L) },
+    0x1e to { ram.setByteAt(HL, rr(ram.getByteAt(HL))) },
+    0x1f to { A = rr(A) },
+    0x20 to { B = sla(B) },
+    0x21 to { C = sla(C) },
+    0x22 to { D = sla(D) },
+    0x23 to { E = sla(E) },
+    0x24 to { H = sla(H) },
+    0x25 to { L = sla(L) },
+    0x26 to { ram.setByteAt(HL, sla(ram.getByteAt(HL))) },
+    0x27 to { A = sla(A) },
+    0x28 to { B = sra(B) },
+    0x29 to { C = sra(C) },
+    0x2a to { D = sra(D) },
+    0x2b to { E = sra(E) },
+    0x2c to { H = sra(H) },
+    0x2d to { L = sra(L) },
+    0x2e to { ram.setByteAt(HL, sra(ram.getByteAt(HL))) },
+    0x2f to { A = sra(A) },
+    0x30 to {},
+    0x31 to {},
+    0x32 to {},
+    0x33 to {},
+    0x34 to {},
+    0x35 to {},
+    0x36 to {},
+    0x37 to {},
+    0x38 to {},
+    0x39 to {},
+    0x3a to {},
+    0x3b to {},
+    0x3c to {},
+    0x3d to {},
+    0x3e to {},
+    0x3f to {},
+    0x40 to {},
+    0x41 to {},
+    0x42 to {},
+    0x43 to {},
+    0x44 to {},
+    0x45 to {},
+    0x46 to {},
+    0x47 to {},
+    0x48 to {},
+    0x49 to {},
+    0x4a to {},
+    0x4b to {},
+    0x4c to {},
+    0x4d to {},
+    0x4e to {},
+    0x4f to {},
+    0x50 to {},
+    0x51 to {},
+    0x52 to {},
+    0x53 to {},
+    0x54 to {},
+    0x55 to {},
+    0x56 to {},
+    0x57 to {},
+    0x58 to {},
+    0x59 to {},
+    0x5a to {},
+    0x5b to {},
+    0x5c to {},
+    0x5d to {},
+    0x5e to {},
+    0x5f to {},
+    0x60 to {},
+    0x61 to {},
+    0x62 to {},
+    0x63 to {},
+    0x64 to {},
+    0x65 to {},
+    0x66 to {},
+    0x67 to {},
+    0x68 to {},
+    0x69 to {},
+    0x6a to {},
+    0x6b to {},
+    0x6c to {},
+    0x6d to {},
+    0x6e to {},
+    0x6f to {},
+    0x70 to {},
+    0x71 to {},
+    0x72 to {},
+    0x73 to {},
+    0x74 to {},
+    0x75 to {},
+    0x76 to {},
+    0x77 to {},
+    0x78 to {},
+    0x79 to {},
+    0x7a to {},
+    0x7b to {},
+    0x7c to {},
+    0x7d to {},
+    0x7e to {},
+    0x7f to {},
+    0x80 to {},
+    0x81 to {},
+    0x82 to {},
+    0x83 to {},
+    0x84 to {},
+    0x85 to {},
+    0x86 to {},
+    0x87 to {},
+    0x88 to {},
+    0x89 to {},
+    0x8a to {},
+    0x8b to {},
+    0x8c to {},
+    0x8d to {},
+    0x8e to {},
+    0x8f to {},
+    0x90 to {},
+    0x91 to {},
+    0x92 to {},
+    0x93 to {},
+    0x94 to {},
+    0x95 to {},
+    0x96 to {},
+    0x97 to {},
+    0x98 to {},
+    0x99 to {},
+    0x9a to {},
+    0x9b to {},
+    0x9c to {},
+    0x9d to {},
+    0x9e to {},
+    0x9f to {},
+    0xa0 to {},
+    0xa1 to {},
+    0xa2 to {},
+    0xa3 to {},
+    0xa4 to {},
+    0xa5 to {},
+    0xa6 to {},
+    0xa7 to {},
+    0xa8 to {},
+    0xa9 to {},
+    0xaa to {},
+    0xab to {},
+    0xac to {},
+    0xad to {},
+    0xae to {},
+    0xaf to {},
+    0xb0 to {},
+    0xb1 to {},
+    0xb2 to {},
+    0xb3 to {},
+    0xb4 to {},
+    0xb5 to {},
+    0xb6 to {},
+    0xb7 to {},
+    0xb8 to {},
+    0xb9 to {},
+    0xba to {},
+    0xbb to {},
+    0xbc to {},
+    0xbd to {},
+    0xbe to {},
+    0xbf to {},
+    0xc0 to {},
+    0xc1 to {},
+    0xc2 to {},
+    0xc3 to {},
+    0xc4 to {},
+    0xc5 to {},
+    0xc6 to {},
+    0xc7 to {},
+    0xc8 to {},
+    0xc9 to {},
+    0xca to {},
+    0xcb to {},
+    0xcc to {},
+    0xcd to {},
+    0xce to {},
+    0xcf to {},
+    0xd0 to {},
+    0xd1 to {},
+    0xd2 to {},
+    0xd3 to {},
+    0xd4 to {},
+    0xd5 to {},
+    0xd6 to {},
+    0xd7 to {},
+    0xd8 to {},
+    0xd9 to {},
+    0xda to {},
+    0xdb to {},
+    0xdc to {},
+    0xdd to {},
+    0xde to {},
+    0xdf to {},
+    0xe0 to {},
+    0xe1 to {},
+    0xe2 to {},
+    0xe3 to {},
+    0xe4 to {},
+    0xe5 to {},
+    0xe6 to {},
+    0xe7 to {},
+    0xe8 to {},
+    0xe9 to {},
+    0xea to {},
+    0xeb to {},
+    0xec to {},
+    0xed to {},
+    0xee to {},
+    0xef to {},
+    0xf0 to {},
+    0xf1 to {},
+    0xf2 to {},
+    0xf3 to {},
+    0xf4 to {},
+    0xf5 to {},
+    0xf6 to {},
+    0xf7 to {},
+    0xf8 to {},
+    0xf9 to {},
+    0xfa to {},
+    0xfb to {},
+    0xfc to {},
+    0xfd to {},
+    0xfe to {},
+    0xff to {}
   )
   fun tick() {
   }
