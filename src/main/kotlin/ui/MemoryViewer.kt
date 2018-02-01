fa091cfdb65bf3af93e01b5d708d9aea367da75f
Thomas Prebeck <thomas.prebeck@web.de>
2018-02-01 12:28:12 +0100

    memory view

--- src/main/kotlin/ui/MemoryViewer.kt
+++ src/main/kotlin/ui/MemoryViewer.kt
@@ -1,20 +1,34 @@
 package de.prt.gb.ui
 import de.prt.gb.hardware.Machine
 import java.awt.Graphics
-import kotlin.concurrent.timer
+import java.awt.GridLayout
+import java.awt.Dimension
+import javax.swing.JPanel
+import javax.swing.JList
+import javax.swing.JScrollPane
+import kotlin.concurrent.thread
+
+private final class MemoryDisplay : JPanel() {
+  val layout = GridLayout(0, 16)
+  init {
+    setLayout(layout)
+    thread {
+      update()
+    }
+  }
+  fun update() {
+    val pc = Machine.getPC()
+    val ram = Machine.getRamImage()
+    val tfs = ram.map{ num ->
+      (num.toInt().toString(16).padStart(2, '0'))
+    }
+    add(JList(tfs.toTypedArray()))
+  }
+}
 
 final class MemoryViewer : ShowableFrame("Memory") {
   init {
-    timer(period=1000L) {
-      println("repaint Memory")
-      repaint()
-    }
-  }
-  override fun paint(g: Graphics) {
-    val pc = Machine.getPC()
-    val ram = Machine.getRamImage()
-    ram.forEachIndexed { i, num ->
-      g.drawString(num.toInt().toString(16).padStart(2, '0'), (i % 16) * 20, (i / 16) * 20)
-    }
+    val scroller = JScrollPane(MemoryDisplay())
+    add(scroller)
   }
 }
